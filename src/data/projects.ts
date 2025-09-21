1e3e44640870613dd29481ef2dec8fb4c916370b
Kiya Rose Ren-Miyakari <kiya.rose@sillylittle.tech>
2025-09-21 16:51:18 -0400

    Add new projects to projects data array
    Added '2025 Class Project' and a placeholder project to the projects array in src/data/projects.ts, expanding the list of showcased projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,4 +12,15 @@ export const projects: Project[] = [
       "A VPN program & documentation focused on secure remote access for small teams.",
     tech: ["ProtonVPN", "OpenVPN", "JavaScript"],
   },
+  {
+      title: "2025 Class Project",
+      description: "Converted an HPE server rack running VMWare to a domain controller and file server using Windows Server 2017",
+      tech: ["HPE", "Windows Server", "VMWare", "ILO"],
+  },
+  {
+      // Placeholder (etc.)
+      title: "...And Hopefully More",
+      descriptions: "Hopefully I will gain more cool things to show off once I progress my career.",
+      tech: ["Hope","Dreams","Passion","Love"],
+  },
 ];
