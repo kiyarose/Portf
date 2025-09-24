1e56fca92ed98b398c4711672a586ca742c4327c
Kiya Rose Ren-Miyakari <kiya.rose@sillylittle.tech>
2025-09-23 23:42:14 -0400

    refactor: Move Colorado Mountain College entry to the top of the educ… (#65)
    * refactor: Move Colorado Mountain College entry to the top of the education timeline
    
    * style: format code with Prettier and StandardJS
    
    This commit fixes the style issues introduced in e55159f according to the output
    from Prettier and StandardJS.
    
    Details: https://github.com/kiyarose/Portf/pull/65
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/data/education.ts
+++ src/data/education.ts
@@ -7,15 +7,15 @@ export type EducationEntry = {
 
 export const educationTimeline: EducationEntry[] = [
   {
-    school: "Greater Altoona CTC",
-    program: "Computer and Networking Technology",
-    dates: "2019 — Jun 2025",
-    tech: ["Networking", "Windows", "Cisco", "Linux"],
-  },
-  {
     school: "Colorado Mountain College",
     program: "Medical Billing and Coding",
     dates: "2025 – 2026",
     tech: ["Medical Coding", "Health Informatics", "Excel"],
+  },
+  {
+    school: "Greater Altoona CTC",
+    program: "Computer and Networking Technology",
+    dates: "2019 — Jun 2025",
+    tech: ["Networking", "Windows", "Cisco", "Linux"],
   },
 ];
